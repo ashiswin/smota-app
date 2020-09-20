2ba9cdc0357c27f0c6bd1bfbaa95d2f956c59dc5
Isaac Ashwin <ashiswin@gmail.com>
2020-09-20 13:33:34 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return <Text>Hello world</Text>;
+  }
+}
+
+export default App;
