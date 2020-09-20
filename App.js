3e679d10b8c93888f4a1510fea0a82dddc639f0a
Isaac Ashwin <ashiswin@gmail.com>
2020-09-20 14:17:46 +0800

    Added bottom bar icons for tabs
    ## Changes
    1. Imported Ionicons for bottom bar icons.
    2. Wired them up using tab navigator prop

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import HomeScreen from './src/screens/HomeScreen';
 import DailyReadingScreen from './src/screens/DailyReadingsScreen';
 import BulletinsScreen from './src/screens/BulletinsScreen';
@@ -20,7 +22,31 @@ class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Daily Readings') {
+                iconName = focused ? 'book' : 'book-outline';
+              } else if (route.name === 'Bulletins') {
+                iconName = focused ? 'newspaper' : 'newspaper-outline';
+              } else if (route.name === 'More') {
+                iconName = focused
+                  ? 'ellipsis-horizontal'
+                  : 'ellipsis-horizontal-outline';
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Daily Readings" component={DailyReadingScreen} />
           <Tab.Screen name="Bulletins" component={BulletinsScreen} />
